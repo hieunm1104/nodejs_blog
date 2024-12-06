4304ce8ef2d9bc7d24c9bfc42cfa061f553d2d67
Hieu <nmh110499@gmail.com>
2024-12-06 23:48:39 +0700

    update

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -59,6 +59,18 @@ class CourseController {
             .then(() => res.redirect('back'))
             .catch(next);
     }
+    action(req, res, next) {
+        console.log(req.body);
+        switch (req.body.action) {
+            case 'delete':
+                Course.delete({ _id: { $in: req.body.courseIds } })
+                    .then(() => res.redirect('back'))
+                    .catch(next);
+                break;
+            default:
+                res.json({ message: 'Action is invalid!' });
+        }
+    }
 }
 
 module.exports = new CourseController();
--- src/routes/courses.route.js
+++ src/routes/courses.route.js
@@ -6,6 +6,7 @@ const courseController = require('../app/controllers/CourseController');
 // newsController.index
 router.get('/create', courseController.create);
 router.post('/store', courseController.store);
+router.post('/action', courseController.action);
 router.get('/:id/edit', courseController.edit);
 router.put('/:id', courseController.update);
 router.patch('/:id/restore', courseController.restore);
